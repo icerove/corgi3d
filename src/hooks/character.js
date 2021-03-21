f828c22ce607e3cf64da1e4634cf385dade0ebc6
Yifang Ma <yifangma93@gmail.com>
2021-03-20 23:22:56 -0700

    fix character

--- src/hooks/character.js
+++ src/hooks/character.js
@@ -1,72 +1,36 @@
-import { useReducer, useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import Quotes from '../assets/quotes/quotes.json';
 const randomColor = require('randomcolor');
 const generate = require('project-name-generator');
 
 const TotalQuotes = Quotes.quotes;
 
-const initialState = {
-  name: generate({ words: 2, alliterative: true }).spaced,
-  quote: null,
-  color: randomColor(),
-  backgroundColor: randomColor(),
-};
+let name = generate({ words: 2, alliterative: true }).spaced
 
-const characterReducer = (currentState, action) => {
-  switch (action.type) {
-    case 'NAME':
-      return {
-        ...currentState,
-        name: action.name,
-      };
-    case 'QUOTE':
-      return {
-        ...currentState,
-        quote: action.quote,
-      };
-    case 'COLOR':
-      return {
-        ...currentState,
-        color: action.color,
-      };
-    case 'BACKGROUND_COLOR':
-      return {
-        ...currentState,
-        backgroundColor: action.backgroundColor,
-      };
-    case 'CLEAR':
-      return initialState;
-    default:
-      throw new Error('Should not come here, something is wrong!');
-  }
-};
+const [_name, setNameIn] = useState(name)
+const [_quote, setQuoteIn] = useState(null)
+const [_color, setColorIn] =useState(randomColor())
+const [_backgroundColor, setBackgroundIn] = useState(randomColor())
 
 const useCharacter = () => {
-  const [characterState, dispatchCharacter] = useReducer(
-    characterReducer,
-    initialState
-  );
-  const clear = useCallback(() => dispatchCharacter({ type: 'CLEAR' }), []);
 
-  const setName = (name) => dispatchCharacter({ type: 'NAME', name });
+  const setName = (name) => setNameIn(name);
 
-  const setColor = (color) => dispatchCharacter({ type: 'COLOR', color });
+  const setColor = (color) => setColorIn(color)
 
-  const setBackgroundColor = (backgroundColor) =>
-    dispatchCharacter({ type: 'BACKGROUND_COLOR', backgroundColor });
+  const setBackgroundColor = (backgroundColor) => setBackgroundIn(backgroundColor)
 
   const setQuote = useCallback(() => {
     let randomNumber = Math.floor(Math.random() * TotalQuotes.length + 1);
     let quote = TotalQuotes[randomNumber].quote;
-    dispatchCharacter({ type: 'QUOTE', quote });
+    setQuoteIn(quote)
   }, []);
 
   return {
-    name: characterState.name,
-    color: characterState.color,
-    backgroundColor: characterState.backgroundColor,
-    quote: characterState.quote,
-    clear,
+    name: _name,
+    color: _color,
+    backgroundColor: _backgroundColor,
+    quote: _quote,
     setName,
     setColor,
     setBackgroundColor,
